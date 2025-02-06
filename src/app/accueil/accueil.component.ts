fd46a8d867ae17bad3bed33e0e694784b26c2b17
Constantin8585 <gadegbeabednego@gmail.com>
2025-02-06 17:59:46 +0000

    Implémentation de acceuil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
 
 @Component({
   selector: 'app-accueil',
-  imports: [ CommonModule ],
+  imports: [CommonModule, FullCalendarModule],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.css'
+  styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
+  abonnementsActifs: any[] = [
+    { nom: 'Abonnement 1', date: '2025-02-01' },
+    { nom: 'Abonnement 2', date: '2025-01-25' },
+    { nom: 'Abonnement 3', date: '2025-01-20' },
+    { nom: 'Abonnement 4', date: '2025-01-15' },
+    { nom: 'Abonnement 5', date: '2025-01-10' }
+  ];
 
+  abonnementsInactifs: any[] = [
+    { nom: 'Abonnement 6', date: '2025-01-05' },
+    { nom: 'Abonnement 7', date: '2025-01-01' },
+    { nom: 'Abonnement 8', date: '2024-12-25' },
+    { nom: 'Abonnement 9', date: '2024-12-20' },
+    { nom: 'Abonnement 10', date: '2024-12-15' }
+  ];
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth'
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
 }
